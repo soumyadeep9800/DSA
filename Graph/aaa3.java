e5e76a642a08791348e9549ef877499935d85cd4
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-10-11 11:48:49 +0530

    add DFS

--- Graph/aaa3.java
+++ Graph/aaa3.java
@@ -68,3 +68,79 @@ public class aaa3 {
         System.out.println();
     }
 }
+//dfs iterative
+// import java.util.ArrayList;
+// import java.util.Stack;
+
+// public class DFSIterative {
+//     public static class Edge {
+//         int src, dest;
+//         public Edge(int s, int d) {
+//             this.src = s;
+//             this.dest = d;
+//         }
+//     }
+
+//     public static void createGraph(ArrayList<Edge>[] graph) {
+//         for (int i = 0; i < graph.length; i++) {
+//             graph[i] = new ArrayList<>();
+//         }
+
+//         graph[0].add(new Edge(0, 1));
+//         graph[0].add(new Edge(0, 2));
+
+//         graph[1].add(new Edge(1, 0));
+//         graph[1].add(new Edge(1, 3));
+
+//         graph[2].add(new Edge(2, 0));
+//         graph[2].add(new Edge(2, 4));
+
+//         graph[3].add(new Edge(3, 1));
+//         graph[3].add(new Edge(3, 4));
+//         graph[3].add(new Edge(3, 5));
+
+//         graph[4].add(new Edge(4, 2));
+//         graph[4].add(new Edge(4, 3));
+//         graph[4].add(new Edge(4, 5));
+
+//         graph[5].add(new Edge(5, 3));
+//         graph[5].add(new Edge(5, 4));
+//         graph[5].add(new Edge(5, 6));
+
+//         graph[6].add(new Edge(6, 5));
+//     }
+
+//     public static void dfsIterative(ArrayList<Edge>[] graph, int start) {
+//         boolean[] visited = new boolean[graph.length];
+//         Stack<Integer> stack = new Stack<>();
+
+//         stack.push(start);
+
+//         while (!stack.isEmpty()) {
+//             int curr = stack.pop();
+
+//             if (!visited[curr]) {
+//                 visited[curr] = true;
+//                 System.out.print(curr + " ");
+                
+//                 // Push neighbors in reverse order to maintain traversal order
+//                 for (int i = graph[curr].size() - 1; i >= 0; i--) {
+//                     Edge e = graph[curr].get(i);
+//                     if (!visited[e.dest]) {
+//                         stack.push(e.dest);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         int V = 7;
+//         ArrayList<Edge>[] graph = new ArrayList[V];
+//         createGraph(graph);
+
+//         System.out.print("DFS (Iterative): ");
+//         dfsIterative(graph, 0);
+//     }
+// }
+
